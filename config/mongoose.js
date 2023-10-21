e4eaddbb2ab98537717786e40cee8cace3658617
Mansi-Sharma06 <0609ms.sharma@gmail.com>
2023-10-21 14:03:18 +0530

    contact done

--- config/mongoose.js
+++ config/mongoose.js
@@ -17,8 +17,9 @@
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
+
 mongoose
-  .connect("mongodb://localhost:27017/contact_list_db", {
+  .connect("mongodb://127.0.0.1:27017/contact_list_db", {
     // useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
